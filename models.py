4efb9e213ae9978c57b1b8984449ad699f8f27e8
michaelbuzek <imac@303.ch>
2025-04-16 07:25:03 +0200

    Fix: TriggerEvent Modell ergänzt

--- models.py
+++ models.py
@@ -1,12 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class TriggerEvent(db.Model):
+    __tablename__ = 'trigger_events'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    setup_name = db.Column(db.String(100))
-    cpe = db.Column(db.String(100))
-    wlan = db.Column(db.String(100))
-    setup_type = db.Column(db.String(100))
+    auftrag_id = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.Text, nullable=False)  # setup-check, fw-download, run-test
+    execute_at = db.Column(db.DateTime, nullable=False)
+    setup = db.Column(db.Text)
+    firmware = db.Column(db.Text)
+    router = db.Column(db.Text)
+    testsets = db.Column(db.ARRAY(db.Text))  # z. B. ['wlan', 'security']
+    status = db.Column(db.Text, default='open')  # open, running, done, error
+    result_log = db.Column(db.Text)
+    report_url = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
